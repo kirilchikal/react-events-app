5940b10a893037b336bd6415d503874d085f4348
Alena <kirilchik.al@gmail.com>
2022-08-25 11:33:21 +0200

    form adjustment with Formik

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    {text: 'Drinks', value: 'drinks'},
+    {text: 'Music', value: 'music'},
+    {text: 'Culture', value: 'culture'},
+    {text: 'Food', value: 'food'},
+    {text: 'Film', value: 'film'},
+    {text: 'Travel', value: 'travel'},
+]
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app/layout/App';
 import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { Router } from 'react-router-dom';
